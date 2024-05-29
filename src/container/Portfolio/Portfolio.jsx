3286ee2ccc971464f9c71d5cf21c2701c8c5e057
Krish <krishnayadav48065@gmail.com>
2024-05-29 13:53:36 +0530

    Project section refactoring

--- src/container/Portfolio/Portfolio.jsx
+++ src/container/Portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Portfolio.css';
-import {images} from '../../constants';
 
+import {Project } from './ProjectsData'
 
 const PortfolioSection = () => {
 
@@ -19,120 +19,62 @@ const PortfolioSection = () => {
         Javascript Projects
       </h2>
       <div className="portfolios">
-        <div className="portfolio-item">
-          <div className="p-image">
-            <img className="portf-img" src={images.project1} alt="" />
-          </div>
-          <div className="hover-items">
-            <h3 className="hover-item-h3">BLOG WEBSITE </h3>
-            <h3 className="hover-item-h3 tex"> PROTOTYPE</h3>
-            <div className="icons">
-              <a href="https://github.com/Krish1rst/codealpha_BlogWebsite"target="_blank"className="icon">
-              <i className="fab fa-github icon-i "></i>
-              </a>
-              <a href="https://krish1rst.github.io/codealpha_BlogWebsite/"target="_blank"className="icon">
-              <i className="fas fa-eye"></i>
-              </a>              
-            </div>
+      {Project.map((item) => {
+  if (item.projectType === "js") {
+    return (
+      <div className="portfolio-item" key={item.id}>
+        <div className="p-image">
+          <img className="portf-img" src={item.image} alt="" />
+        </div>
+        <div className="hover-items">
+          <h3 className="hover-item-h3">{item.title}</h3>
+          <h3 className="hover-item-h3 tex">{item.subTitle}</h3>
+          <div className="icons">
+            <a href={item.gitHubLink} target="_blank" rel="noreferrer" className="icon">
+              {item.icon1}
+            </a>
+            <a href={item.liveLink} target="_blank" rel="noreferrer" className="icon">
+              {item.icon2}
+            </a>
           </div>
         </div>
-        <div className="portfolio-item">
-          <div className="p-image">
-            <img className="portf-img" src={images.project2} alt="" />
-          </div>
-
-          <div className="hover-items">
-            <h3 className="hover-item-h3">PORTFOLIO WEBSITE </h3>
-            <h3 className="hover-item-h3 tex">PORTFOLIO PROTOTYPE</h3>
-            <div className="icons">
-              <a href="https://github.com/Krish1rst/Portfolio"target="_blank"className="icon">
-              <i className="fab fa-github icon-i "></i>
-              </a>
-              <a href="https://krish1rst.github.io/Portfolio/"target="_blank"className="icon">
-              <i className="fas fa-eye"></i>
-              </a>              
-            </div>
-          </div>
-        </div>
-        <div className="portfolio-item">
-          <div className="p-image">
-            <img className="portf-img" src={images.project3} alt="" />
-          </div>
-
-          <div className="hover-items">
-            <h3 className="hover-item-h3">RESUME BUILDER</h3>
-            <div className="icons">
-               <a href="https://github.com/Krish1rst/ResumeBuilder"target="_blank"className="icon">
-              <i className="fab fa-github icon-i "></i>
-              </a>
-              <a href="https://krish1rst.github.io/ResumeBuilder/"target="_blank"className="icon">
-              <i className="fas fa-eye"></i>
-              </a>              
-            </div>
-          </div>
-        </div>
-        
-        
       </div>
+    );
+  }
+  return null;
+})}
+</div>
+
       <h2 className="port-text-heading">
         React Projects
       </h2>
+
       <div className="portfolios">
-      <div className="portfolio-item">
-          <div className="p-image">
-            <img className="portf-img" src={images.project4} alt="" />
-          </div>
-
-          <div className="hover-items">
-            <h3 className="hover-item-h3">TEXTUTILS</h3>
-            <h3 className="hover-item-h3 tex">TEXT MANIPULATION PROJECT </h3>
-            <div className="icons">
-            <a href="https://github.com/Krish1rst/TextUtils_Krishna"target="_blank"className="icon">
-              <i className="fab fa-github icon-i "></i>
-              </a>
-              <a href="https://spectacular-youtiao-b8c139.netlify.app/"target="_blank"className="icon">
-              <i className="fas fa-eye"></i>
-              </a>              
-            </div>
-          </div>
+      {Project.map((item) => {
+  if (item.projectType === "react") {
+    return (
+      <div className="portfolio-item" key={item.id}>
+        <div className="p-image">
+          <img className="portf-img" src={item.image} alt="" />
         </div>
-        <div className="portfolio-item">
-          <div className="p-image">
-            <img className="portf-img" src={images.project5} alt="" />
-          </div>
-
-          <div className="hover-items">
-            <h3 className="hover-item-h3">NewsDaily</h3>
-            <h3 className="hover-item-h3 tex">NEWS API PROJECT</h3>
-            <div className="icons">
-              <a href="https://github.com/Krish1rst/NewsDaily"target="_blank"className="icon">
-              <i className="fab fa-github icon-i "></i>
-              </a>
-              <a href="#"className="icon">
-              <i className="fas fa-eye"></i>
-              </a>              
-            </div>
-          </div>
-        </div>
-        <div className="portfolio-item">
-          <div className="p-image">
-            <img className="portf-img" src={images.project6} alt="" />
-          </div>
-
-          <div className="hover-items">
-            <h3 className="hover-item-h3">PORTFOLIO</h3>
-            <h3 className="hover-item-h3 tex">MY PERSONAL PORTFOLIO </h3>
-            <div className="icons">
-              <a href="#"className="icon">
-              <i className="fab fa-github icon-i "></i>
-              </a>
-              <a href="#"className="icon">
-              <i className="fas fa-eye"></i>
-              </a>              
-            </div>
+        <div className="hover-items">
+          <h3 className="hover-item-h3">{item.title}</h3>
+          <h3 className="hover-item-h3 tex">{item.subTitle}</h3>
+          <div className="icons">
+            <a href={item.gitHubLink} target="_blank" rel="noreferrer" className="icon">
+              {item.icon1}
+            </a>
+            <a href={item.liveLink} target="_blank" rel="noreferrer" className="icon">
+              {item.icon2}
+            </a>
           </div>
         </div>
       </div>
+    );
+  }
+  return null;
+})}
+</div>
     </section>
   );
 }
---
+++ src/container/Portfolio/ProjectsData.jsx
@@ -0,0 +1,68 @@
+import {images} from '../../constants';
+
+const {project1,project2,project3,project4,project5,project6}=images;
+export const Project = [
+    { id: 1, 
+        projectType: "js",
+       image : project1,
+       title: "BLOG WEBSITE",
+       subTitle: "PROTOTYPE",
+       gitHubLink : "https://github.com/Krish1rst/codealpha_BlogWebsite",
+       liveLink :"https://krish1rst.github.io/codealpha_BlogWebsite/",
+       icon1 : <i className="fab fa-github icon-i "></i>,
+       icon2: <i className="fas fa-eye"></i> 
+     },
+     { id: 2, 
+        projectType: "js",
+        image : project2,
+        title: "PORTFOLIO WEBSITE",
+        subTitle: "PORTFOLIO PROTOTYPE",
+        gitHubLink : "https://github.com/Krish1rst/Portfolio",
+        liveLink :"https://krish1rst.github.io/Portfolio/",
+        icon1 : <i className="fab fa-github icon-i "></i>,
+        icon2: <i className="fas fa-eye"></i> 
+      },
+      { id: 3, 
+        projectType: "js",
+        image : project3,
+        title: "RESUME BUILDER",
+        subTitle: "",
+        gitHubLink : "https://github.com/Krish1rst/ResumeBuilder",
+        liveLink :"https://krish1rst.github.io/ResumeBuilder/",
+        icon1 : <i className="fab fa-github icon-i "></i>,
+        icon2: <i className="fas fa-eye"></i> 
+      },
+      { id: 4, 
+        projectType: "react",
+       image : project4,
+       title: "TEXTUTILS",
+       subTitle: "TEXT MANIPULATION PROJECT ",
+       gitHubLink : "https://github.com/Krish1rst/TextUtils_Krishna",
+       liveLink :"https://spectacular-youtiao-b8c139.netlify.app/",
+       icon1 : <i className="fab fa-github icon-i "></i>,
+       icon2: <i className="fas fa-eye"></i> 
+     },
+     { id: 5, 
+        projectType: "react",
+        image : project5,
+        title: "NewsDaily",
+        subTitle: "NEWS API PROJECT",
+        gitHubLink : "https://github.com/Krish1rst/NewsDaily",
+        liveLink :"",
+        icon1 : <i className="fab fa-github icon-i "></i>,
+        icon2: <i className="fas fa-eye"></i> 
+      },
+      { id: 6, 
+        projectType: "react",
+        image : project6,
+        title: "PORTFOLIO",
+        subTitle: "MY PERSONAL PORTFOLIO",
+        gitHubLink : "https://github.com/Krish1rst/ReactPortfolio",
+        liveLink :"#",
+        icon1 : <i className="fab fa-github icon-i "></i>,
+        icon2: <i className="fas fa-eye"></i> 
+      },
+    
+  ];
+
+
